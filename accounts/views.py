ae6fa2d408c84cdb08dd6ae29d999e09c94bc8dc
taesu-park <tsoo1014@gmail.com>
2020-08-29 15:40:36 +0900

    200829 | add login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm 
+
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        forms = AuthenticationForm(request, request.POST)
+        if forms.is_valid():
+            user = forms.get_user()
+            auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect(request.GET.get('next') or 'accounts:index')
+    else:
+        forms = AuthenticationForm()
+    context = {
+        'forms': forms
+    }
+    return render(request, 'accounts/login.html', context)
